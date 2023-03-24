f5c20e1ee15c7820d4f5d86cf4f969dd42dba104
Uzytkownik <kutik93@gmail.com>
2023-03-25 00:06:19 +0100

    The model layer has been created and the class responsible for the book is in it.

---
+++ src/main/java/pl/sda/extraproject/bookstore/model/Book.java
@@ -0,0 +1,58 @@
+package pl.sda.extraproject.bookstore.model;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String photoUrl;
+    private Integer isbn;
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public Integer getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Integer isbn) {
+        this.isbn = isbn;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
