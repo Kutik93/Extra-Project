7331731983a9b086ca8d74c6b14916e59010cb51
Uzytkownik <kutik93@gmail.com>
2023-03-28 23:32:45 +0200

    Update last commit.

---
+++ src/main/front/bookstore/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+
+import {Container, Col, Row} from 'react-bootstrap';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import NavigationBar from './components/NavigationBar'
+import Footer from './components/Footer';
+import Welcome from './components/Welcome';
+import Book from './components/Book';
+import BookList from './components/BookList';
+
+function App() {
+    const marginTop ={
+    marginTop:"20px"
+    }
+
+  return (
+    <Router>
+    <NavigationBar/>
+    <Container>
+    <Row>
+        <Col lg={12} style={marginTop}>
+            <Switch>
+                     <Route path="/" exact component={Welcome}/>
+                     <Route path="/add" exact component={Book}/>
+                      <Route path="/list" exact component={BookList}/>
+                      <Route path="/edit/:id" exact component={Book}/>
+            </Switch>
+        </Col>
+        </Row>
+    </Container>
+
+    <Footer/>
+
+    </Router>
+  );
+}
+
+export default App;
